39750019c2a5de958436c7ca7ddcb8c9e63f988b
Abhinav Somasani <abhinavsomasani@gmail.com>
2026-07-06 18:20:39 +0530

    Time: 3 ms (100%), Space: 46.5 MB (43.52%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        int n = intervals.length;
+        int cnt = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j && intervals[i][0]>=intervals[j][0] && intervals[i][1]<=intervals[j][1]){
+                    cnt++;
+                    break;
+                }
+            }
+        }
+        return n-cnt;
+    }
+}
