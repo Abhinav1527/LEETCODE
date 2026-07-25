b68d3d1eb1ce2a529c51656a0412484ade905af3
Abhinav Somasani <abhinavsomasani@gmail.com>
2026-07-25 20:15:44 +0530

    Time: 12 ms (37.16%), Space: 52.4 MB (90.76%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,14 +3,14 @@ class Solution {
         Map<Integer,Integer> m = new HashMap<>();
         int n = nums.length;
         int res = 0;
-        int maj = 0;
         for(int num : nums){
             m.put(num,m.getOrDefault(num,0)+1);
-            if(m.get(num)>maj){
-                res = num;
-                maj = m.get(num);
+        }
+        for(Map.Entry<Integer,Integer> e : m.entrySet()){
+            if(e.getValue()>n/2){
+                return e.getKey();
             }
         }
-        return res;
+        return 0;
     }
 }
