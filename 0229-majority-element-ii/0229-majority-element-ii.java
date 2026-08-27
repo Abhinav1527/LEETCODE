e3944df362853451b8bbc312069b03e9fdb679b2
Abhinav Somasani <abhinavsomasani@gmail.com>
2026-08-27 20:35:40 +0530

    Time: 2 ms (99.95%), Space: 52.4 MB (65.38%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,16 +1,47 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> l = new ArrayList<>();
-        Map<Integer, Integer> hm = new HashMap<>();
         int n = nums.length;
+        int c1 = 0;
+        int c2 = 0;
+        int e1 = 0;
+        int e2 = 0;
 
-        for (int i = 0; i < n; i++) {
-            hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1);
-            if (hm.get(nums[i]) > (n / 3) && !l.contains(nums[i])) {
-                l.add(nums[i]);
+        for(int i=0;i<n;i++){
+            if(c1 == 0 && e2 != nums[i]){
+                c1 = 1;
+                e1 = nums[i];
+            }else if(c2 == 0 && e1 != nums[i]){
+                c2++;
+                e2 = nums[i];
+            }else if(nums[i] == e1){
+                c1++;
+            }else if(nums[i] == e2){
+                c2++;
+            }else{
+                c1--;
+                c2--;
             }
-            if(l.size() == 2) break;
         }
+        c1 = 0;
+        c2 = 0;
+
+        for(int i=0;i<n;i++){
+            if(nums[i] == e1){
+                c1++;
+            }else if(nums[i] == e2){
+                c2++;
+            }
+        }
+
+        if(c1>n/3){
+            l.add(e1);
+        }
+
+        if(c2>n/3){
+            l.add(e2);
+        }
+
         return l;
     }
 }
